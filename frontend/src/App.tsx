73f4eeaf2d2e0464baaea8681c1181b346e962dd
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-07-15 02:56:45 +0300

    fetching notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Note } from "./models/note";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const loadNotes = async () => {
+      try {
+        const res = await fetch("/api/notes", {
+          method: "GET",
+        });
+        const notes = await res.json();
+        setNotes(notes);
+      } catch (err) {
+        console.error(err);
+        alert(err);
+      }
+    };
+    loadNotes();
+  }, []);
+
+  return <div className="App">{JSON.stringify(notes)}</div>;
 }
 
 export default App;
