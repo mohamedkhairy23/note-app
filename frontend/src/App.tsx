727a0f3cf1b76b05b15ebcbd9b52d1e98aee360a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-07-15 21:51:00 +0300

    Fetching Notes Data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Note } from "./models/note";
+import { useEffect, useState } from "react";
+import { Note as NoteModel } from "./models/note";
+import Note from "./componets/Note";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "./styles/NotesPage.module.css";
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<NoteModel[]>([]);
 
   useEffect(() => {
     const loadNotes = async () => {
@@ -22,7 +23,17 @@ function App() {
     loadNotes();
   }, []);
 
-  return <div className="App">{JSON.stringify(notes)}</div>;
+  return (
+    <Container>
+      <Row xs={1} md={2} xl={3} className="justify-content-md-center g-4">
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
