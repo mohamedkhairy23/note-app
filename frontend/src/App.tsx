12e28563c0a15730cfd1880df160e0a885dbec01
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-07-16 22:04:23 +0300

    create note functionality in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import { Note as NoteModel } from "./models/note";
 import Note from "./componets/Note";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import styles from "./styles/NotesPage.module.css";
-
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from "./componets/AddNoteDialog";
+import styleUtils from "./styles/utils.module.css";
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     const loadNotes = async () => {
       try {
-        const res = await fetch("/api/notes", {
-          method: "GET",
-        });
-        const notes = await res.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (err) {
         console.error(err);
@@ -25,6 +25,12 @@ function App() {
 
   return (
     <Container>
+      <Button
+        className={`mb-4 ${styleUtils.blockCenter}`}
+        onClick={() => setShowAddNoteDialog(true)}
+      >
+        Add new note
+      </Button>
       <Row xs={1} md={2} xl={3} className="justify-content-md-center g-4">
         {notes.map((note) => (
           <Col key={note._id}>
@@ -32,6 +38,15 @@ function App() {
           </Col>
         ))}
       </Row>
+      {showAddNoteDialog && (
+        <AddNoteDialog
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote]);
+            setShowAddNoteDialog(false);
+          }}
+        />
+      )}
     </Container>
   );
 }
---
+++ frontend/src/network/notes_api.ts
@@ -0,0 +1,35 @@
+import { Note } from "../models/note";
+
+async function fetchData(input: RequestInfo, init?: RequestInit) {
+  const response = await fetch(input, init);
+  if (response.ok) {
+    return response;
+  } else {
+    const errorBody = await response.json();
+    const errorMessage = errorBody.console.error;
+    throw Error(errorMessage);
+  }
+}
+
+export async function fetchNotes(): Promise<Note[]> {
+  const res = await fetch("/api/notes", {
+    method: "GET",
+  });
+  return res.json();
+}
+
+export interface NoteInput {
+  title: string;
+  text?: string;
+}
+
+export async function createNote(note: NoteInput): Promise<Note> {
+  const res = await fetchData("/api/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+  return res.json();
+}
